ddb7f5ff0e7794d96ac96508fa7ad175dd2f7308
shokah <shokah.s@gmail.com>
2020-01-26 15:30:08 +0200

    create scale invarient loss
    on label space, need to check if it's any good

--- utils/loss.py
+++ utils/loss.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+
 class SegmentationLosses(object):
     def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False):
         self.ignore_index = ignore_index
@@ -50,6 +51,65 @@ class SegmentationLosses(object):
 
         return loss
 
+
+class DepthLosses(object):
+    def __init__(self, weight=None, size_average=True, batch_average=True, ignore_index=255, cuda=False, num_class=255):
+        self.ignore_index = ignore_index
+        self.weight = weight
+        self.size_average = size_average
+        self.batch_average = batch_average
+        self.cuda = cuda
+        self.num_class = num_class
+
+    def build_loss(self, mode='depth_loss'):
+        """Choices: ['depth_loss' or 'depth_lod']"""
+        if mode == 'depth_loss':
+            return self.DepthLoss
+        elif mode == 'depth_lod':
+            return self.DepthLODLoss
+        else:
+            raise NotImplementedError
+
+    def DepthLoss(self, predict, target):
+        '''
+        scale invarient depth on label space
+        :param predict: network output
+        :param target: dataset label
+        :return:
+        '''
+        lamda = 1.0
+        n, c, h, w = predict.size()
+        di = (target - predict) / self.num_class
+        k = h * w
+        di2 = torch.pow(di, 2)
+        first_term = torch.sum(di2, (1, 2, 3)) / k
+        second_term = torch.pow(torch.sum(di, (1, 2, 3)), 2) / (k ** 2)
+        loss = first_term - lamda * second_term
+
+        if self.batch_average:
+            loss /= n
+
+        return loss
+
+    def DepthLODLoss(self, logit, target, gamma=2, alpha=0.5):
+        n, c, h, w = logit.size()
+        criterion = nn.CrossEntropyLoss(weight=self.weight, ignore_index=self.ignore_index,
+                                        size_average=self.size_average)
+        if self.cuda:
+            criterion = criterion.cuda()
+
+        logpt = -criterion(logit, target.long())
+        pt = torch.exp(logpt)
+        if alpha is not None:
+            logpt *= alpha
+        loss = -((1 - pt) ** gamma) * logpt
+
+        if self.batch_average:
+            loss /= n
+
+        return loss
+
+
 if __name__ == "__main__":
     loss = SegmentationLosses(cuda=True)
     a = torch.rand(1, 3, 7, 7).cuda()
@@ -57,7 +117,3 @@ if __name__ == "__main__":
     print(loss.CrossEntropyLoss(a, b).item())
     print(loss.FocalLoss(a, b, gamma=0, alpha=None).item())
     print(loss.FocalLoss(a, b, gamma=2, alpha=0.5).item())
-
-
-
-
